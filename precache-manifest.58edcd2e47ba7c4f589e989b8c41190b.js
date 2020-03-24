a98b4f2f2d80864dfe751cb8041bf2fccd0f2102
Dmitriy <skachok.d@gmail.com>
2020-03-24 12:19:07 +0200

    Updates

---
+++ precache-manifest.58edcd2e47ba7c4f589e989b8c41190b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06064366b5ce6f94c0b73a85ead51ef0",
+    "url": "/make-me-up/index.html"
+  },
+  {
+    "revision": "965152f5d5aa911e1ed8",
+    "url": "/make-me-up/static/css/main.fee9dd48.chunk.css"
+  },
+  {
+    "revision": "750544c75dd50c2f466f",
+    "url": "/make-me-up/static/js/2.866af65c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/make-me-up/static/js/2.866af65c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "965152f5d5aa911e1ed8",
+    "url": "/make-me-up/static/js/main.b5d36b60.chunk.js"
+  },
+  {
+    "revision": "79ec9206920a3d819f36",
+    "url": "/make-me-up/static/js/runtime-main.2148bb48.js"
+  },
+  {
+    "revision": "a8944e85cb10ff850e5b9e0579173b43",
+    "url": "/make-me-up/static/media/background.a8944e85.png"
+  }
+]);
